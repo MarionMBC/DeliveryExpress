1d2eb65aa13fff63ed615f0f1015b3c42e0b4f22
MarionMBC <marion.bustamante@unah.hn>
2022-10-24 17:44:36 -0600

    Listo

--- js/controlador.js
+++ js/controlador.js
@@ -2,4 +2,20 @@ var scene = document.getElementById('scene');
 console.log(scene);
 var parallaxInstance = new Parallax(scene);
 console.log(parallaxInstance);
-console.log("hola")
+console.log("Parallax listo.");
+
+function facebook () {
+    window.open("https://www.facebook.com/profile.php?id=100070947710040","_blank");
+}
+
+function instagram () {
+    window.open("https://www.instagram.com/","_blank");
+}
+
+function twitter () {
+    window.open("https://twitter.com/","_blank");
+}
+
+function youtube () {
+    window.open("https://www.youtube.com/","_blank");
+}
